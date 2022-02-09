f712ec23d8642a0d45b6a95b3431f84ec22da691
18alantom <2.alan.tom@gmail.com>
2022-02-09 12:39:33 +0530

    center stuff, some more boilerplate

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1 class="text-gray-700 text-center mt-12 text-3xl">Hello, World!</h1>
+  <h1 class="text-gray-700 mt-12 text-3xl">Hello, World!</h1>
 </template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+<style>
+#app {
+  @apply text-center justify-center h-screen flex flex-col;
+}
+</style>
