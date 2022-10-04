ec8f141e0eb85e4b00856ae31c3fcb2d9d13138c
18alantom <2.alan.tom@gmail.com>
2022-10-04 17:36:36 +0530

    bump versions

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref('Hello, World');
+defineExpose({ message });
+</script>
 <template>
   <div>
     <h1 class="text-slate-700">{{ message }}</h1>
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      message: 'Hello, World',
-    };
-  },
-};
-</script>
--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  content: ['./index.html', './src/**/*.{vue,js,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
